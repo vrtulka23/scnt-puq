8f921c98d0eae7e2bbac6add6ca00e416ddfae87
vrtulka23 <vrtulka23@pm.me>
2024-10-06 01:26:32 +0200

    added missing config file

---
+++ src/config.h
@@ -0,0 +1,23 @@
+/*
+ *  Numerical precision
+ */
+#define MAGNITUDE_PRECISION     double
+#define EXPONENT_INT_PRECISION  int
+#define EXPONENT_REAL_PRECISION double
+
+/*
+ *  Modules
+ */
+#define MAGNITUDE_ERRORS
+#define MAGNITUDE_ARRAYS
+#define EXPONENT_FRACTIONS
+
+#define UNITS_TEMPERATURES
+#define UNITS_LOGARITHMIC
+
+#define UNIT_SYSTEM_CGS
+#define UNIT_SYSTEM_NUS
+#define UNIT_SYSTEM_EUS
+
+#define PREPROCESS_SYSTEM
+#define PREPROCESS_SYMBOLS
